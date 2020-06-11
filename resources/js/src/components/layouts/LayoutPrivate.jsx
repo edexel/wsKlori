5ac68cf7d68f8e7d89a4966136b255b7e1874f29
Joel Valdivia <jooelvaldivi@gmail.com>
2020-06-11 18:09:59 -0500

    [ADD] se agrega sidebar y layout publico y privado

---
+++ resources/js/src/components/layouts/LayoutPrivate.jsx
@@ -0,0 +1,47 @@
+/**
+ * Created by Joel Valdivia
+ * Date 11 Jun 2020
+ * Description: Layout privado aqui empieza a maquetar la interfaz
+ */
+
+import React from "react";
+import NavBarCustom from "../NavBarCustom";
+import ModalCustom from "../ModalCustom";
+import LoadingScreen from 'react-loading-screen';
+import { useSelector } from "react-redux";
+import { Container, Row, Col } from "react-bootstrap";
+import Sidebar  from "../../components/Sidebar";
+
+function LayoutPrivate({ Component, route, history }) {
+
+    // obtiene del store las opciones del modal
+    const { loading } = useSelector(store => store);
+
+    return (
+        <LoadingScreen
+            loading={loading.show}
+            bgColor='#f1f1f1'
+            spinnerColor='#9ee5f8'
+            textColor='#676767'
+            text={loading.text}
+        >
+            <NavBarCustom />
+            <Container fluid>
+                <Row>
+                    <Col xs={2} id="sidebar-wrapper">
+                    <Sidebar/>
+                    </Col>
+                    <Col xs={10} id="page-content-wrapper">
+                        <Component route={route} history={history} />
+                    </Col>
+                </Row>
+
+            </Container>
+            {/* BEGIN Modal global de la APP */}
+            <ModalCustom />
+            {/* END Modal global de la APP */}
+        </LoadingScreen>
+    )
+}
+
+export default LayoutPrivate;
